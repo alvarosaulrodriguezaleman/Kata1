c8d4b429e59bc91dab0862a6d755c45b42853fe7
Álvaro Saúl Rodríguez Alemán <>
2018-09-21 11:24:15 +0100

    Kata1 v3.0 - Ahora usamos las librerías LocalDate y Period para el manejo de fechas, en vez de Calendar y GregorianCalendar. - Eliminada la constante tipo long MILLISECONDS_PER_YEAR y el método privado milliSecondsToYear() al ya no ser necesarios.

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,13 +1,10 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 public class Kata1 {
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1966, 6, 4);
+        LocalDate date = LocalDate.of(1966, 6, 4);
         
         Person person = new Person("Pepe", date);
         System.out.println(person.getName() + " tiene " + person.getAge() 
